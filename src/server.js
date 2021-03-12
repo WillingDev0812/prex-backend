e290e7c74053fd0042c295dd90841ff8924d098b
WillingDev0812 <greendev1004@gmail.com>
2021-03-12 03:07:45 -0500

    heroku

--- src/server.js
+++ src/server.js
@@ -14,82 +14,110 @@ const run = async () => {
   //   })
   //   .catch(console.log);
 
+
   // let date = new Date();
   // date.setSeconds(date.getSeconds() + 10);
   // let minute = date.getMinutes();
   // let second = date.getSeconds();
-
   // var initJob = new CronJob(second + ' ' + minute + '/9 * * * *', async () => {
-  //   try {
-  //     let startTime = Date.now() + 55;
-  //     console.log(">>>>>>>>Clearing market...");
-  //     await api.clearMarket();
-  //     console.log(">>>>>>>>Initializing market...");
-  //     await api.initMarket(startTime);
-  //   }
-  //   catch(e) {
-  //     console.log(e);
-  //   }
+    
   // });
-  // var startJob = new CronJob(second + ' ' + ((minute+1)%60) + '/9 * * * *', async () => {
-  //   try {
-  //     console.log(">>>>>>>>Starting market...");
-  //     await api.startMarket();
-  //   }
-  //   catch(e) {
-  //     console.log(e);
-  //   }
+  // var startJob = new CronJob(second + ' ' + ((minute+1)%60) + '/9 * * * *', () => {
+   
+  //   startFunc();
   // });
-  // var endJob = new CronJob(second + ' ' + ((minute+7)%60) + '/9 * * * *', async () => {
-  //   try {
-  //     console.log(">>>>>>>>Ending market...");
-  //     api.endMarket();
+  // var endJob = new CronJob(second + ' ' + ((minute+7)%60) + '/9 * * * *', () => {
+  //   const endFunc = async () => {
+  //     try {
+  //       console.log(">>>>>>>>Ending market...");
+  //       api.endMarket();
+  //     }
+  //     catch(e) {
+  //       console.log(e);
+  //       setTimeout(endFunc, 12000);
+  //     }
   //   }
-  //   catch(e) {
-  //     console.log(e);
-  //   }
+  //   endFunc();
   // });
-  // initJob.start();
-  // startJob.start();
-  // endJob.start();
-  // var m = 0;
-  // setInterval(() => {
-  //   console.log(m);
-  //   m++;
-  // }, 1000);
 
-  
-  let m = 1;
-  setInterval(() => {
-    console.log(m++);
-  }, 1000);
+  const init_start = 60;
+  const start_predict = 180;
+  const end_init = 60;
+  const retry = 12;
 
-  const mainfunc = async () => {
+  const initFunc = async () => {
     try {
-      let startTime = Math.floor(Date.now()/1000) + 30;
-      setTimeout(async () => {
-        console.log(">>>>>>>>>>Starting market...");
-        let res = await api.startMarket();
-        console.log(">>>>>>>>>>Done.");
-      }, 32000);
-      setTimeout(async () => {
-        console.log(">>>>>>>>>>Ending market...");
-        await api.endMarket();
-        console.log(">>>>>>>>>>Done.");
-      }, 332000);
-      console.log(">>>>>>>>>>Clearing market...");
+      console.log(">>>>>>>>Clearing market...", Date.now()/1000);
       await api.clearMarket();
-      console.log(">>>>>>>>>>Initializing market...");
-      await api.initMarket(startTime, 150, 5);
-      console.log(">>>>>>>>>>Done.");
+      let startTime = Math.floor(Date.now()/1000) + init_start;
+      console.log(">>>>>>>>Initializing market...", Date.now()/1000);
+      await api.initMarket(startTime, start_predict, 5);
+      setTimeout(startFunc, init_start*1000);
     }
-    catch (e) {
+    catch(e) {
       console.log(e);
+      setTimeout(initFunc, retry*1000);
     }
   }
 
-  setInterval(mainfunc, 360000);
-  mainfunc();
+  const startFunc = async () => {
+    try {
+      console.log(">>>>>>>>Starting market...", Date.now()/1000);
+      await api.startMarket();
+      setTimeout(endFunc, start_predict*2*1000);
+    }
+    catch(e) {
+      console.log(e);
+      setTimeout(startFunc, retry*1000);
+    }
+  }
+
+  const endFunc = async () => {
+    try {
+      console.log(">>>>>>>>Ending market...", Date.now()/1000);
+      api.endMarket();
+      setTimeout(initFunc, end_init*1000);
+    }
+    catch(e) {
+      console.log(e);
+      setTimeout(endFunc, retry*1000);
+    }
+  }
+  
+  initFunc();
+
+
+  // let m = 1;
+  // setInterval(() => {
+  //   console.log(m++);
+  // }, 1000);
+
+  // const mainfunc = async () => {
+  //   try {
+  //     let startTime = Math.floor(Date.now()/1000) + 30;
+  //     setTimeout(async () => {
+  //       console.log(">>>>>>>>>>Starting market...");
+  //       let res = await api.startMarket();
+  //       console.log(">>>>>>>>>>Done.");
+  //     }, 32000);
+  //     setTimeout(async () => {
+  //       console.log(">>>>>>>>>>Ending market...");
+  //       await api.endMarket();
+  //       console.log(">>>>>>>>>>Done.");
+  //     }, 332000);
+  //     console.log(">>>>>>>>>>Clearing market...");
+  //     await api.clearMarket();
+  //     console.log(">>>>>>>>>>Initializing market...");
+  //     await api.initMarket(startTime, 150, 5);
+  //     console.log(">>>>>>>>>>Done.");
+  //   }
+  //   catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // setInterval(mainfunc, 360000);
+  // mainfunc();
 
   
   // console.log(">>>>>>>>>>Ending market...");
